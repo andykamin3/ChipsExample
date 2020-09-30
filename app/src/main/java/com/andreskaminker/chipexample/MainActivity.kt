1e36c2d7440d35b2097f3a40872c8bd3ebc766fe
Andrés Kaminker <andykamin3@gmail.com>
2020-09-30 00:06:52 -0300

    Updated images
    Signed-off-by: Andrés Kaminker <andykamin3@gmail.com>

--- app/src/main/java/com/andreskaminker/chipexample/MainActivity.kt
+++ app/src/main/java/com/andreskaminker/chipexample/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.andreskaminker.chipexample
 
+import android.app.Application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.view.children
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.andreskaminker.chipexample.databinding.ActivityMainBinding
 import java.lang.IllegalArgumentException
 
-
 lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(this.layoutInflater)
@@ -16,7 +20,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onStart() {
-        binding.buttonChecker.setOnClickListener {
+        binding.chipGroup.children.forEach { view->
+            view.setOnClickListener {
             val checkedList = binding.chipGroup.checkedChipIds.map {
                 return@map when(it){
                     R.id.chipArms ->{
@@ -34,7 +39,9 @@ class MainActivity : AppCompatActivity() {
                     else -> throw IllegalArgumentException("The int required was not in the Enum")
                 }
             }
-            Log.d(TAG, checkedList.toString())
+                Log.d(TAG, checkedList.toString())
+                binding.textView.text = checkedList.toString()
+            }
         }
         super.onStart()
     }
